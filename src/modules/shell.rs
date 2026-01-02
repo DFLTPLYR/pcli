be7b8a02b5df2d7f09f03c17226dae3c7f6c6c57
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-01-02 20:19:22 +0800

    refactor(shell): unify compositor actions and cleanup
    Refactored shell module to consolidate redundant functions for
    compositor
    actions into a single function, reducing code duplication. Updated logic
    to make it easier to extend support for additional compositors like
    Hyprland and MangoWC in the future. Added comments for clarity and
    maintainability.

--- src/modules/shell.rs
+++ src/modules/shell.rs
@@ -20,42 +20,26 @@ impl DesktopEnvironment {
 
 pub fn shell_query(target: &LaunchTarget) {
     match target {
-        LaunchTarget::WallpaperPicker => wallpaper_picker(),
-        LaunchTarget::AppLauncher => println!("Quickshell: Launching App Launcher"),
-        LaunchTarget::ExtendedBar => extended_bar(),
+        LaunchTarget::WallpaperPicker => compositor_action("toggleWallpaperPicker"),
+        LaunchTarget::AppLauncher => compositor_action("toggleAppLauncher"),
+        LaunchTarget::ExtendedBar => compositor_action("toggleExtendedBar"),
     }
 }
 
-fn extended_bar() {
+// compositor actions based on wayland compositor type
+fn compositor_action(action: &str) {
     match DesktopEnvironment::from_env() {
         DesktopEnvironment::Niri => {
-            let mut socket = Socket::connect().expect("Failed to connect to Niri socket");
-            let reply = socket.send(Request::FocusedWindow);
             let qs = is_qs_running();
             if qs {
-                println!("{:?}", reply);
-                qs_ipc_caller(&"toggleExtendedBar".to_string());
+                qs_ipc_caller(&action.to_string());
             }
         }
         DesktopEnvironment::Unknown => { /* handle others */ }
     }
 }
 
-fn wallpaper_picker() {
-    match DesktopEnvironment::from_env() {
-        DesktopEnvironment::Niri => {
-            let mut socket = Socket::connect().expect("Failed to connect to Niri socket");
-            let reply = socket.send(Request::FocusedWindow);
-            let qs = is_qs_running();
-            if qs {
-                println!("{:?}", reply);
-                qs_ipc_caller(&"toggleWallpaperPicker".to_string());
-            }
-        }
-        DesktopEnvironment::Unknown => { /* handle others */ }
-    }
-}
-
+// Check if 'qs' process is running
 fn is_qs_running() -> bool {
     Command::new("pgrep")
         .arg("-x")
@@ -65,6 +49,7 @@ fn is_qs_running() -> bool {
         .unwrap_or(false)
 }
 
+// a wrapper to call qs ipc commands
 fn qs_ipc_caller(props: &String) {
     if let Some(path) = get_config_path() {
         let rel_path = home_relative(&path)
@@ -81,6 +66,7 @@ fn qs_ipc_caller(props: &String) {
     }
 }
 
+// make the path relative to home directory
 fn home_relative(path: &str) -> String {
     if let Ok(home) = std::env::var("HOME")
         && path.starts_with(&home)
@@ -90,6 +76,7 @@ fn home_relative(path: &str) -> String {
     path.to_string()
 }
 
+// get the config path from 'qs list --all' command
 fn get_config_path() -> Option<String> {
     let output = Command::new("qs").arg("list").arg("--all").output().ok()?;
     let stdout = String::from_utf8_lossy(&output.stdout);
